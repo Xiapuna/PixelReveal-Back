98428cbd78cae21679cca2e6bb3fa571bbbe4628
Célia <celia.godfrin@gmail.com>
2026-09-17 15:54:06 +0200

    Initial commit: Spring Boot project skeleton

---
+++ src/main/java/com/celiagodfrin/pixelrevealback/PixelRevealBackApplication.java
@@ -0,0 +1,13 @@
+package com.celiagodfrin.pixelrevealback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelRevealBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelRevealBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/celiagodfrin/pixelrevealback/PixelRevealBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celiagodfrin.pixelrevealback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelRevealBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
